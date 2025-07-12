90ec4a4026a2f72f43fa63e7f645fbe8ba446c7d
aman.yadav <aman.yadav@swiggy.in>
2025-07-12 15:06:09 +0530

    Update GitHub Pages configuration for aman24x7 repository

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
+  base: '/aman24x7/',
   build: {
     outDir: 'dist',
     sourcemap: false,
